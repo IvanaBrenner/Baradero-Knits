beacb1b45b3aa80f4f34ebcd186a339153ab90e0
Ivana Brenner <ivanabrenner@gmail.com>
2023-12-07 15:54:58 -0500

    Primer commit - prueba arrays

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+console.log ("Baradero Knits Test");
+
+const mediasAngoraMujer = ("Medias Angora Mujer");
+const mediasAngoraHombre = ("Medias Angora Hombre");
+const mediasAngoraBaby = ("Medias Angora Baby");
+const saquitoCrochet = ("Saquito al Crochet");
+ 
+const arrayCatalogo = [mediasAngoraMujer, mediasAngoraHombre, mediasAngoraBaby, saquitoCrochet];
+const arrayCarrito = [];
+
+console.log (arrayCatalogo);
+console.log(arrayCatalogo[2]);
