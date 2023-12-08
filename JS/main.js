6fe6bdda2e481239f9181e6189eacbc254636cac
Ivana Brenner <ivanabrenner@gmail.com>
2023-12-07 22:06:24 -0500

    Agrego push a los arrays, corrijo condicionales

--- JS/main.js
+++ JS/main.js
@@ -14,11 +14,15 @@ console.log (arrayCatalogo.length); */
 
 // 1 - MOSTRAR CATALOGO AL USUARIO
 
-alert ("Este es nuestro calatogo de productos: \n" + mediasAngoraMujer + "\n" + mediasAngoraHombre + "\n" + mediasAngoraBaby + "\n" + saquitoCrochet );
+function mostrarCatalogo(params) {
+    alert ("Este es nuestro calatogo de productos: \n" + mediasAngoraMujer + "\n" + mediasAngoraHombre + "\n" + mediasAngoraBaby + "\n" + saquitoCrochet );
 
 for (let index = 0; index < arrayCatalogo.length; index++) {
     alert ("Te ofrecemos " + arrayCatalogo[index]);
 }
+}
+
+mostrarCatalogo ();
 
 // 2 - INVITACION A ELEGIR PRODUCTOS
 
@@ -26,8 +30,10 @@ let chosenProduct1 = prompt ("Elige un producto");
 
 // 3 - SUMAR PRODUCTO AL CARRITO
 
-if (chosenProduct1 = true) {
+if (chosenProduct1 != null) {
+    arrayCarrito.push (chosenProduct1);
     //pushear producto a arrayCarrito
+    alert (chosenProduct1 + " se ha agregado a tu carrito!");
 } else {
     alert ("Gracias por visitar Baradero Knits! \nTe esperamos de nuevo pronto!");
 }
@@ -38,20 +44,44 @@ alert ("Si llevas dos productos, tienes un 10% de descuento sobre el segundo pro
 
 let chosenProduct2 = prompt ("Si quieres aprovechar la promo, elige otro producto, si no, presiona cancel");
 
-if (chosenProduct2 = true) {
+if (chosenProduct2 != null) {
+    arrayCarrito.push (chosenProduct2);
+    alert (chosenProduct2 + " se ha agregado a tu carrito!");
     //pushear segundo producto a arrayCarrito
+} else {
+    confirm ("Ir al carrito")
 }
 
 // 5 - GO TO CART (MOSTRAR CARRITO)
 
+function mostrarCarrito(params) {
+    for (let index = 0; index < arrayCarrito.length; index++) {
+        alert ("Tienes en tu carrito " + arrayCarrito[index]);
+    }
+}
+
+mostrarCarrito ();
+
 // 6 - SUMAR / MOSTRAR TOTAL DE LA COMPRA
 
+
 let totalCompra;
-alert ("El total de tu compra es " + totalCompra);
+
+function sumar(params) {
+    totalCompra = (chosenProduct1 + chosenProduct2);
+}
+
+function mostrarTotalCompra(params) {
+    alert ("El total de tu compra es " + totalCompra);
+}
+
+
 
 // 7 - REALIZAR LA TRANSACCION
 
-let transaccionCompleta = prompt ("Desear confirmar tu compra?");
+let transaccionCompleta = confirm ("Desear confirmar tu compra?");
+
+
 if (transaccionCompleta = true) {
     alert ("Muchas gracias por tu compra!")
 } else {
